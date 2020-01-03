a1aa66700480d7dbb1ff02169c290076b0930a94
Ali Mengutay <alozicyber@gmail.com>
2020-01-03 13:32:39 +0100

    Updates

---
+++ precache-manifest.5d1f51a905260493d295ddaddb4a64f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72489fcab8ad8903f7caa3d11a77044",
+    "url": "/UReact2020Monsters/index.html"
+  },
+  {
+    "revision": "383cf16aa736f1a68261",
+    "url": "/UReact2020Monsters/static/css/main.d3c9546c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/UReact2020Monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/UReact2020Monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "383cf16aa736f1a68261",
+    "url": "/UReact2020Monsters/static/js/main.31c70c76.chunk.js"
+  },
+  {
+    "revision": "dd58e6bab836e120de62",
+    "url": "/UReact2020Monsters/static/js/runtime-main.88217eb9.js"
+  }
+]);
